93560ef414359125140bb5dd81ab34da561c68f7
dominatordq <dominatordq@gmail.com>
2020-12-28 10:59:34 -0700

    Extracted Sentry logger service to separate js file

--- http-app/src/services/httpService.js
+++ http-app/src/services/httpService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import * as Sentry from "@sentry/react";
+import logger from './logService'
 import { toast } from 'react-toastify';
 
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
   
     if (!expectedError) {
-        Sentry.captureException(error);
+        logger.log(error);
         toast.error('An unexpected error occurred.');
     }
   
